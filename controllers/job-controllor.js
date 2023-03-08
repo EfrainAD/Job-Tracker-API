67041d82bfb32a44c2f1e736adc71aa35acf47e8
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-03-08 17:53:46 -0500

    added functioning routes to react a job and get all jobs in the DB

--- controllers/job-controllor.js
+++ controllers/job-controllor.js
@@ -1,4 +1,11 @@
+import Job from "../models/job-model.js";
+//TODO: handle errors
 // GET
+export const createJob = async (req, res) => {
+   const job = await Job.create(req.body)
+   res.status(201).json(job)
+}
 export const getJobs = async (req, res) => {
-   res.json('hi, in controllor now')  
+   const job = await Job.find({})
+   res.status(201).json(job)
 }
--- routes/job-routes.js
+++ routes/job-routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { getJobs } from '../controllers/job-controllor.js';
+import { createJob, getJobs } from '../controllers/job-controllor.js';
 const router = express.Router()
 
 //Routes
+router.post('/', createJob)
 router.get('/', getJobs)
 
  export default router;
