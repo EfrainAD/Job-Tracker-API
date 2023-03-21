2881d5f6a68a9ed999c1b07a404adaa859672d38
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-03-21 13:40:21 -0400

    1. npm i express-async-handler 2. Used it in cob-controllor

--- controllers/job-controllor.js
+++ controllers/job-controllor.js
@@ -1,11 +1,18 @@
-import Job from "../models/job-model.js";
-//TODO: handle errors
-// GET
-export const createJob = async (req, res) => {
+import Job from '../models/job-model.js'
+import asyncHandler from 'express-async-handler'
+
+// Create Job
+export const createJob = asyncHandler(async (req, res, next) => {
+   if (!req.body.companyName) {
+      res.status(400)
+      throw new Error('Missing one or more required fields')
+   }
    const job = await Job.create(req.body)
    res.status(201).json(job)
-}
-export const getJobs = async (req, res) => {
+})
+
+// Get All Jobs
+export const getJobs = asyncHandler(async (req, res) => {
    const job = await Job.find({})
    res.status(201).json(job)
-}
+})
