0a71d65880463d5cb113b12158d71bfde8220078
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-04-06 15:05:43 -0400

    Fixed that a change I made didn't work with other ports of the codebase so I made some changes to work whti creating the user and updating. (that reated to password)

--- models/user-model.js
+++ models/user-model.js
@@ -1,67 +1,99 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcryptjs'
 
-const userSchema = mongoose.Schema({
-     name: {
-          type: String,
-          required: [true, `User's name is required`]
-     },
-     email: {
-          type: String,
-          required: [true, `User's email is required`],
-          unique: true,
-          trim: true,
-          match: [
-               /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
-               "Email not valid"
-          ],
-     },
-     password: {
-          type: String,
-          required: [true, `A user's password is required`],
-          minLength: [60, 'Encripted password must be at least 60 characters long'],
-     },
-     photo: {
-          type: String,
-          required: [true, 'Must have a photo, Error should have a default'],
-          default: 'https://i.ibb.co/4pDNDk1/avatar.png'
-     },
-     phone: {
-          type: String,
-          default: '+1'
-     },
-     bio: {
-          type: String,
-          maxLength: [250, 'Bio must be shorter then 250 characters long'],
-          default: 'bio'
-     }
-}, {
-     toJSON: {
-       transform: function(doc, ret) {
-         delete ret.password;
-         return ret;
-       }
-     },
-     timestamps: true
-})
+const userSchema = mongoose.Schema(
+   {
+      name: {
+         type: String,
+         required: [true, `User's name is required`],
+      },
+      email: {
+         type: String,
+         required: [true, `User's email is required`],
+         unique: true,
+         trim: true,
+         match: [
+            /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
+            'Email not valid',
+         ],
+      },
+      password: {
+         type: String,
+         required: [true, `A user's password is required`],
+         minLength: [
+            60,
+            'Encripted password must be at least 60 characters long',
+         ],
+      },
+      photo: {
+         type: String,
+         required: [true, 'Must have a photo, Error should have a default'],
+         default: 'https://i.ibb.co/4pDNDk1/avatar.png',
+      },
+      phone: {
+         type: String,
+         default: '+1',
+      },
+      bio: {
+         type: String,
+         maxLength: [250, 'Bio must be shorter then 250 characters long'],
+         default: 'bio',
+      },
+   },
+   {
+      toJSON: {
+         transform: function (doc, ret) {
+            delete ret.password
+            return ret
+         },
+      },
+      timestamps: true,
+   }
+)
 
 // Encrypt Password
-userSchema.pre('findOneAndUpdate', async function(next) {
-     const update = this.getUpdate()
-   
-     if (!update.password || typeof update.password !== 'string') {
-          return next()
-     }
+userSchema.pre('validate', async function (next) {
+   try {
+      if (this.isModified('password') || this.isNew) {
+         const salt = await bcrypt.genSalt(10)
+         const hashedPassword = await bcrypt.hash(this.password, salt)
+         this.password = hashedPassword
+      }
+      next()
+   } catch (error) {
+      next(error)
+   }
+})
 
-     try {
-          const salt = await bcrypt.genSalt(10)
-          const hashedPassword = await bcrypt.hash(update.password, salt)
+// userSchema.pre('save', async function (next) {
+//    try {
+//       console.log('hi pass')
+//       if (this.isModified('password')) {
+//          const salt = await bcrypt.genSalt(10)
+//          const hashedPassword = await bcrypt.hash(this.password, salt)
+//          this.password = hashedPassword
+//       }
+//       next()
+//    } catch (error) {
+//       next(error)
+//    }
+// })
+userSchema.pre('findOneAndUpdate', async function (next) {
+   const update = this.getUpdate()
 
-          this.getUpdate().password = hashedPassword
-          next()
-     } catch (error) {
-          return next(error)
-     }
+   if (!update.password || typeof update.password !== 'string') {
+      return next()
+   }
+
+   try {
+      const salt = await bcrypt.genSalt(10)
+      const hashedPassword = await bcrypt.hash(update.password, salt)
+
+      this.getUpdate().password = hashedPassword
+      next()
+   } catch (error) {
+      return next(error)
+   }
 })
 
 export default mongoose.model('User', userSchema)
