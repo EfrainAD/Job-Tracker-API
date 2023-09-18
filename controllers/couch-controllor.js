12b484129eb56113368db10c8cb51166198d2ae8
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-09-18 13:44:00 -0400

    change: Changed the way couches where added, and removed the limit on how many a person can have
    1. removed mongoDB transaction
    2. Simply now add's the doc and send a successful true

--- controllers/couch-controllor.js
+++ controllers/couch-controllor.js
@@ -5,14 +5,12 @@ import Couch from '../models/couch-model.js'
 import User from '../models/user-model.js'
 import { checkIfUserExists } from '../utils/user/user-utils.js'
 import { throwError } from '../utils/errorHandler/errorHandler-utils.js'
-import { COUCHES_LIMIT } from '../utils/variables/globalVariables.js'
 
 // Add User's Couch
 export const addUserCouch = asyncHandler(async (req, res) => {
    const user = req.user
    const { _id } = user
    const email = req.body.email
-   const numberOfCouches = user?.couches?.length
 
    if (!user) {
       throwError(500, `Server Error: Improper use of get user's info function`)
@@ -21,53 +19,24 @@ export const addUserCouch = asyncHandler(async (req, res) => {
       throwError(400, `You need the the couch's email`)
    }
 
-   if (numberOfCouches >= COUCHES_LIMIT) {
-      throwError(
-         400,
-         `Sorry, the limit of couches is ${COUCHES_LIMIT}, the number of couches you have is ${numberOfCouches}`
-      )
-   }
-
    const newCouch = await User.findOne({ email })
 
    if (!checkIfUserExists(newCouch))
       throwError(400, `Couch not found, double check the email provided`)
 
-   // Start Transaction
-   const session = await mongoose.startSession()
-   session.startTransaction()
+   const find = await Couch.find({ couch: newCouch._id, couchee: _id })
 
-   try {
-      // Update the requesting user's couch field
-      const usersCouches = await User.findOneAndUpdate(
-         { _id },
-         { $addToSet: { couches: newCouch._id } },
-         { session, new: true }
-      )
-         .select('-_id couches')
-         .populate({ path: 'couches', select: 'name' })
-
-      // Update the couch's field for the user being added as a person being couched
-      const search = await Couch.find({ couch: newCouch._id, couchee: _id })
-      if (search.length === 0) {
-         await Couch.create({
-            couch: newCouch._id,
-            couchee: _id,
-            active: false,
-         })
-      }
-
-      await session.commitTransaction()
-      session.endSession()
-
-      res.json({
-         usersCouches: usersCouches.couches,
+   if (find.length === 0) {
+      await Couch.create({
+         couch: newCouch._id,
+         couchee: _id,
+         active: false,
       })
-   } catch (error) {
-      await session.abortTransaction()
-      session.endSession()
-      throwError(500, `Transaction aborted: ${error}`)
    }
+
+   res.json({
+      successful: true,
+   })
 })
 
 // Remove User's Couch
